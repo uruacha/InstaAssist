d88d4d6e271b8731fe864ed5e0a12ae6bbc50406
uruacha <k.kazu.rmr1@gmail.com>
2025-12-08 23:14:53 +0900

    Fix: Add assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/InstaAssist",
+  assetPrefix: "/InstaAssist/",
   trailingSlash: true,
   images: {
     unoptimized: true,
